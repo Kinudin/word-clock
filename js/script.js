37891c04857cf4e812fd60ea6398aa52688eddba
Eric Cross <crosse88@gmail.com>
2015-08-21 23:08:46 -0700

    Starting brranch

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-/**
- * Created by eric.cross on 8/21/2015.
- */
+// Utilize local storage to store background colors, text color, or font choice
 
 // Setting up the ID variables to select during the change hours function
+
+/*
+* Issues:
+    * Digital Clock on sidebar wasn't updating correctly
+        *> Solved
+        *> currentTime was only being declared once, moved
+        *> var currentTime = new Date(); into updateClock()
+        *> so it would update every startClock()
+    * Word clock wouldn't update when currentHour > 12
+        *> Added || hours == currentHour+12
+    * Need to create a better way to cycle through the IDs.
+
+*/
 
 var one     = document.querySelector('#one'),
     two     = document.querySelector('#two'),
@@ -18,27 +29,22 @@ var one     = document.querySelector('#one'),
     twelve  = document.querySelector('#twelve');
 
 
-
 document.addEventListener('DOMContentLoaded', startClock);
 function startClock(){
-    var startClockTime = new Date(),
-        h = startClockTime.getHours(),
-        m = startClockTime.getMinutes(),
-        s = startClockTime.getSeconds();
-    startClockTime = formatHour(h) + ":" + padZeros(m,2) + ":" + padZeros(s,2);
     setInterval(updateClock, 1000);
-    setInterval(changeHours, 1000);
     updateClock();
 }
 
 function updateClock(){
+    // Update the digital clock in upper left corner
+    // with the correct format. Steal 
     var currentTime = new Date(),
         h = currentTime.getHours(),
         m = currentTime.getMinutes(),
         s = currentTime.getSeconds();
     currentTime = formatHour(h) + ":" + padZeros(m,2) + ":" + padZeros(s,2);
     document.querySelector('#digital-clock').innerHTML = currentTime;
-    changeHours(h, m, s);
+    changeHours(h, m, s, currentTime);
 }
 
 function padZeros(num, size){
@@ -46,49 +52,48 @@ function padZeros(num, size){
     return s.substr(s.length - size);
 }
 
-function changeHours(hours, minutes, seconds){
-    var h = formatHour(hours);
-        m = padZeros(minutes,2);
-        s = padZeros(seconds,2);
-    console.log('Hours: ' + h + " Minutes: " + m + " Seconds: " + s);
+function changeHours(hours, minutes, seconds, timeString){
+    console.log('Hours: ' + hours + " Minutes: " + minutes + " Seconds: " + seconds + " timeString: " + timeString);
 
-    if(h == 1){
+    if(hours == 1 || hours == 13){
         one.className = "current-time";
         twelve.className.replace( /(?:^|\s)current-time(?!\S)/g , '' );
-    } else if (h == 2){
+    } else if (hours == 2 || hours == 14){
         two.className = "current-time";
         one.className.replace( /(?:^|\s)current-time(?!\S)/g , '' );
-    } else if (h == 3){
+    } else if (hours == 3 || hours == 15){
         three.className = "current-time";
         two.className.replace( /(?:^|\s)current-time(?!\S)/g , '' );
-    } else if (h == 4){
+    } else if (hours == 4 || hours == 16){
         four.className = "current-time";
         three.className.replace( /(?:^|\s)current-time(?!\S)/g , '' );
-    } else if (h == 5){
+    } else if (hours == 5 || hours == 17){
         five.className = "current-time";
         four.className.replace( /(?:^|\s)current-time(?!\S)/g , '' );
-    } else if (h == 6){
+    } else if (hours == 6 || hours == 18){
         six.className = "current-time";
         five.className.replace( /(?:^|\s)current-time(?!\S)/g , '' );
-    } else if (h == 7){
+    } else if (hours == 7 || hours == 19){
         seven.className = "current-time";
         six.className.replace( /(?:^|\s)current-time(?!\S)/g , '' );
-    } else if (h == 8){
-        eight.className = "current-time";
+    } else if (hours == 8 || hours==20){
+        eight.className = "third current-time";
         seven.className.replace( /(?:^|\s)current-time(?!\S)/g , '' );
-    } else if (h == 9){
-        nine.className = "current-time";
+    } else if (hours == 9 || hours == 21){
+        nine.className = "third current-time";
         eight.className.replace( /(?:^|\s)current-time(?!\S)/g , '' );
-    } else if (h == 10){
-        ten.className = "current-time";
+    } else if (hours == 10 || hours == 22){
+        ten.className = "third current-time";
         nine.className.replace( /(?:^|\s)current-time(?!\S)/g , '' );
-    } else if (h == 11){
+    } else if (hours == 11 || hours == 23){
         eleven.className = "current-time";
         ten.className.replace( /(?:^|\s)current-time(?!\S)/g , '' );
-    } else if (h == 12){
+    } else if (hours == 12 || hours == 0){
         twelve.className = "current-time";
         eleven.className.replace( /(?:^|\s)current-time(?!\S)/g , '' );
     }
+
+    console.log(hours + typeof(hours));
 
 }
 
